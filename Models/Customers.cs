1a6606dd40b10da759fa18672230939d95b1f12a
Daniel Farias <jdanieel70@hotmail.com>
2025-07-11 11:18:24 -0600

    FEAT: AÑADIDO COLUMNA DE EMAIL Y PASSWORD A CLIENTES

--- Models/Customers.cs
+++ Models/Customers.cs
@@ -12,6 +12,12 @@ namespace API_Shop_Online.Models
         public string LastName { get; set; }
         [Required]
         public string Address { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
         public DateTime CreatedAt { get; set; }
         public ICollection<Sale> Sales { get; set; }
     }
